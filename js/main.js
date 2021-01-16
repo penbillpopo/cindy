eb6fd266f176306edec4da1e373079d9ecf9f16a
penbillpopo <penbillpopo@gmail.com>
2021-01-16 19:09:31 +0800

    第一個commit

---
+++ js/main.js
@@ -0,0 +1,50 @@
+window.sr = ScrollReveal({
+    reset: false,
+    distance: '0px',
+    scale: 1,
+    easing: 'ease-in-out',
+    duration: 800,
+    viewFactor: 0
+});
+sr.reveal('.reveal_bottom', {
+    origin: 'bottom'
+});
+sr.reveal('.reveal_bottom_d300', {
+    origin: 'bottom',
+    delay: 300
+});
+sr.reveal('.reveal_left', {
+    origin: 'left'
+});
+sr.reveal('.reveal_left_d300', {
+    origin: 'left',
+    delay: 300
+});
+sr.reveal('.reveal_left_d600', {
+    origin: 'left',
+    delay: 600
+});
+sr.reveal('.reveal_left_d900', {
+    origin: 'left',
+    delay: 900
+});
+sr.reveal('.reveal_left_d1200', {
+    origin: 'left',
+    delay: 1200
+});
+sr.reveal('.reveal_right', {
+    origin: 'right'
+});
+sr.reveal('.reveal_opacity', {
+    distance: '0px'
+});
+sr.reveal('.reveal_scale', {
+    distance: '0px',
+    scale: 0.9
+});
+sr.reveal('.reveal_delay', {
+    delay: 600
+});
+sr.reveal('.reveal_delay1s', {
+    delay: 1000
+});
